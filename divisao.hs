5024d5072b35f85a6cb680bc0ffb1cbf0062d4d6
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-03-31 15:19:11 -0300

    divisao float

--- divisao.hs
+++ divisao.hs
@@ -1,3 +1,7 @@
 -- essa divisao funciona apenas para numeros inteiros
 divisao :: Int -> Int -> Int
 divisao x y =  x `div` y
+
+--divisao para float
+divisaof :: Float -> Float -> Float
+divisaof x y = x/y
