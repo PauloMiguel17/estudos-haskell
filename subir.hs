8e64531c9afdec9baffe1b745b89e2260510f93b
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-04-02 14:48:49 -0300

    subir

---
+++ subir.hs
@@ -0,0 +1,9 @@
+subir :: Int -> Int -> Int
+subir x y
+  | x == y    = x
+  | x < y     = subir (x + 1) y
+  | otherwise = x
+
+-- subir 3 7 == 7
+-- subir 7 7 == 7
+-- subir 9 5 == 9 (não sobe porque já é maior)
