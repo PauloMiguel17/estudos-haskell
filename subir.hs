70aa6198ee2ea544d1ccba4396a22fc32d3e6b4d
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2026-04-02 14:54:06 -0300

    função diminuir numero

--- subir.hs
+++ subir.hs
@@ -7,3 +7,10 @@ subir x y
 -- subir 3 7 == 7
 -- subir 7 7 == 7
 -- subir 9 5 == 9 (não sobe porque já é maior)
+
+
+diminuir :: Int -> Int -> Int
+diminuir x y
+    | x == y    = x
+    | x > y     = diminuir (x - 1) y
+    | otherwise = x
